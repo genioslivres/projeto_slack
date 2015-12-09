3bac65137247682806998cb0537c059227f6a8af
genioslivres <grupogenioslivres@gmail.com>
2015-12-09 16:14:22 -0200

    Create script_mrocha.sh

---
+++ script_mrocha.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+dialog --msgbox 'Este script vai converter seu(s) arquivo(s) .RPM e transformalos em .TXZ , depois realizara a instalação' 50 50
+dialog --msgbox 'Digite 1 para instalar o arquivo localmente ( o arquivo rpm deve estar na mesma pasta do Script , ou 2 para colar o link de download do arquivo' 50 50
+read NUM
+read LINK
+read USUARIO
+
+case $NUM in
+	1) rpm2txz *.rpm && upgradepkg --install-new *.t?z && rm *.rpm ;;
+	2) cd /home/$USUARIO/Downloads && wget -c $LINK && rpm2txz *.rpm && upgradepkg --install-new *.t?z && rm *.rpm ;
+	*) dialog --msgbox 'Esta opção não é valida , tente novamente' 50 50 "!" ;;
+esac
